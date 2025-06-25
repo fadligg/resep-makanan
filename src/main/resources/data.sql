aa9f0540008c4c18d5c97cfc18ea3a1023c70e47
fadligg <fadli.hn51@gmail.com>
2025-06-25 10:56:09 +0700

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,61 @@
+INSERT INTO resep (judul, kategori, bahan, langkah) VALUES
+
+-- Masakan Indonesia
+('Nasi Goreng Kampung', 'Masakan Indonesia', 
+'1. Nasi putih<br>2. Bawang merah<br>3. Bawang putih<br>4. Cabai rawit<br>5. Terasi<br>6. Telur<br>7. Kecap manis<br>8. Garam<br>9. Minyak goreng', 
+'1. Haluskan bawang merah, bawang putih, cabai rawit, dan sedikit terasi.<br>
+2. Panaskan minyak, tumis bumbu halus hingga harum dan matang.<br>
+3. Tambahkan telur ke dalam wajan, orak-arik hingga matang.<br>
+4. Masukkan nasi putih, aduk rata dengan telur dan bumbu.<br>
+5. Tambahkan kecap manis dan garam sesuai selera.<br>
+6. Aduk terus hingga nasi berwarna merata dan sedikit kering.<br>
+7. Koreksi rasa, sajikan hangat dengan kerupuk dan acar.'),
+
+('Soto Betawi', 'Masakan Jakarta', 
+'1. Daging sapi<br>2. Susu cair<br>3. Santan kental<br>4. Bawang putih<br>5. Bawang merah<br>6. Jahe<br>7. Lengkuas<br>8. Serai<br>9. Daun jeruk<br>10. Tomat<br>11. Kentang<br>12. Emping', 
+'1. Rebus daging sapi hingga empuk, potong-potong sesuai selera.<br>
+2. Haluskan bawang putih, bawang merah, jahe dan tumis bersama lengkuas, serai, dan daun jeruk.<br>
+3. Masukkan bumbu ke dalam air rebusan daging, tambahkan santan dan susu.<br>
+4. Masak dengan api kecil hingga kuah mendidih dan harum.<br>
+5. Goreng kentang dan iris tomat untuk pelengkap.<br>
+6. Sajikan daging dan kuah bersama kentang goreng, tomat, dan emping di atas nasi.'),
+
+-- Masakan Luar Negeri
+('Spaghetti Carbonara', 'Masakan Italia', 
+'1. Spaghetti<br>2. Telur<br>3. Keju parmesan<br>4. Daging asap<br>5. Bawang putih<br>6. Garam<br>7. Merica hitam', 
+'1. Rebus spaghetti hingga al dente, tiriskan dan sisakan sedikit air rebusan.<br>
+2. Kocok telur dengan keju parmesan, garam, dan merica hitam.<br>
+3. Tumis bawang putih dan daging asap hingga garing.<br>
+4. Campurkan spaghetti ke dalam wajan, matikan api.<br>
+5. Tambahkan campuran telur dan keju sambil terus diaduk cepat agar tidak menggumpal.<br>
+6. Tambahkan sedikit air rebusan spaghetti jika terlalu kental.<br>
+7. Sajikan hangat dengan taburan keju tambahan dan lada hitam.'),
+
+('Tteokbokki', 'Masakan Korea Selatan', 
+'1. Kue beras (tteok)<br>2. Odeng (kue ikan)<br>3. Gochujang<br>4. Gula<br>5. Bawang putih<br>6. Kaldu anchovy<br>7. Daun bawang<br>8. Telur rebus', 
+'1. Rebus air dan masukkan kaldu anchovy, masak hingga kaldu keluar.<br>
+2. Tambahkan gochujang dan gula, aduk rata hingga larut.<br>
+3. Masukkan tteok dan odeng, aduk sesekali agar tidak lengket.<br>
+4. Masak dengan api sedang selama 10–15 menit hingga kuah mengental.<br>
+5. Tambahkan bawang putih cincang dan irisan daun bawang.<br>
+6. Masak lagi selama 3 menit.<br>
+7. Sajikan tteokbokki panas dengan telur rebus dan taburan biji wijen.'),
+
+('Chicken Curry India', 'Masakan India', 
+'1. Daging ayam<br>2. Bawang bombay<br>3. Bawang putih<br>4. Jahe<br>5. Tomat<br>6. Santan<br>7. Bubuk kari<br>8. Kunyit<br>9. Jinten<br>10. Ketumbar<br>11. Garam<br>12. Minyak', 
+'1. Tumis bawang bombay, jahe, dan bawang putih hingga layu.<br>
+2. Tambahkan bubuk kari, kunyit, jinten, dan ketumbar.<br>
+3. Masukkan potongan ayam, aduk rata dengan bumbu dan masak hingga berubah warna.<br>
+4. Tambahkan tomat cincang dan masak hingga hancur dan menyatu.<br>
+5. Tuang santan, kecilkan api dan masak hingga ayam empuk dan kuah kental.<br>
+6. Koreksi rasa, tambahkan garam jika perlu.<br>
+7. Sajikan dengan nasi basmati atau roti naan hangat.'),
+
+('Tom Yum Goong', 'Masakan Thailand', 
+'1. Udang besar<br>2. Jamur<br>3. Tomat<br>4. Serai<br>5. Daun jeruk<br>6. Lengkuas<br>7. Cabai<br>8. Saus ikan<br>9. Air jeruk nipis<br>10. Kaldu ayam', 
+'1. Rebus kaldu ayam bersama serai, lengkuas, dan daun jeruk.<br>
+2. Tambahkan cabai, jamur, dan tomat, masak beberapa menit.<br>
+3. Masukkan udang, masak hingga berubah warna.<br>
+4. Tambahkan saus ikan dan air jeruk nipis.<br>
+5. Masak 2 menit lagi, koreksi rasa.<br>
+6. Sajikan panas dengan daun ketumbar di atasnya.');
